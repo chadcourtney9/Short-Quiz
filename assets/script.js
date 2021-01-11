7365ce63168c713d51d18333341c4e178a7df475
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-11 18:01:04 -0500

    fixed timer, still need to prepend

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,15 @@
-// function countdown(elementName, minutes, seconds) {
-//     var element, endTime, hours, mins, msLeft, time;
-
-//     function twoDigits(n) {
-//         return n <= 9 ? "0" + n : n;
-//     }
-
-//     function updateTimer() {
-//         msLeft = endTime - +new Date();
-//         if (msLeft < 1000) {
-//             element.innerHTML = "Time is up!";
-//         } else {
-//             time = new Date(msLeft);
-//             hours = time.getUTCHours();
-//             mins = time.getUTCMinutes();
-//             element.innerHTML =
-//                 (hours ? hours + ":" + twoDigits(mins) : mins) +
-//                 ":" +
-//                 twoDigits(time.getUTCSeconds());
-//             setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
-//         }
-//     }
-
-//     element = document.getElementById(elementName);
-//     endTime = +new Date() + 1000 * (60 * minutes + seconds) + 500;
-//     updateTimer();
-// }
-
-// countdown("countdown", 2, 0);
 var startQuiz = document.getElementsByClassName("start");
 const countdown = document.getElementById('countdown');
 let timeLeft = 120;
+var startButton = document.getElementById("start")
 
 let countdownId;
 // prepend a number to timer. add event listener to startCountdown on click
 //startQuiz.
+
+
+function startGame() {
+}
 
 function startCountdown() {
     return setInterval(function () {
