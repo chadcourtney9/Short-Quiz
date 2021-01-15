fa8d470613ab00fd7d3289e7c2385a808559da5b
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-14 21:24:44 -0500

    set up question generator + answer

--- assets/questions.js
+++ assets/questions.js
@@ -1,44 +1,46 @@
 const countdown = document.getElementById('countdown');
 let timeLeft = 120;
 let countdownId;
-const answerButtons = $("answerButtons");
+const answerButtons = $(".page-item");
+var currentIndex = 0;
 
 // <h5>What is the correct syntax for referring to an external script called "xxx.js"?
 //                         <!--1 <script href="xxx.js" 2. <script src="xxx.js"> 3. <script name="xxx.js"-->
 //                  
-const qHeader = ".questionHeader";
+const questionHeader = $(".questionHeader");
 
 const myQuestions = [
     {
         title: "Question 1:",
         question: "Inside which HTML element do we put the JavaScript?",
-        answers: {
-            a: "<script>",
-            b: "<javascript>",
-            c: "<scripting>",
-            d: "<js>"
-        },
+        answers: [
+            "<script>",
+            "<javascript>",
+            "<scripting>",
+            "<js>"
+        ],
         correctAnswer: "c"
     },
     {
         title: "Question 2:",
         question: "How can you add a comment in a JavaScript?",
-        answers: {
-            a: "'this is a comment",
-            b: "//this is a comment",
-            c: "<!--this is a comment-->"
-        },
+        answers: [
+            "'this is a comment",
+            "//this is a comment",
+            "<!--this is a comment-->",
+            "gergerg"
+        ],
         correctAnswer: "c"
     },
     {
         title: "Question 3:",
         question: "Where is the correct place to insert a JavaScript?",
-        answers: {
-            a: "The <head> section",
-            b: "The <body> section",
-            c: "The <html> section",
-            d: "Both the <head> and <body> are correct"
-        },
+        answers: [
+            "The <head> section",
+            "The <body> section",
+            "The <html> section",
+            "Both the <head> and <body> are correct"
+        ],
         correctAnswer: "d"
     },
     // question: "Which tool can you use to ensure code quality?",
@@ -50,6 +52,7 @@ const myQuestions = [
     // },
     // correctAnswer: "d"
 ];
+
 
 
 function startCountdown() {
@@ -71,24 +74,34 @@ function updateTimer() {
 updateTimer()
 countdownId = startCountdown()
 
-//pseudo code
-
+//pseudo code $ infront to know jquery obj
+// last  q bring to create highscore. deduct 25 seconds per wrong answer.
 // set / push question/answer objects[] 
-function qGen() {
+function qGenerator() {
+    var currentQ = myQuestions[currentIndex];
+    console.log(answerButtons);
+    answerButtons.each(function (i, li) {
+        const $li = $(li)
+        // console.log(currentQ.answers[i])
+        console.log($li)
+        $li.find("a").text(currentQ.answers[i])
+    });
+    questionHeader.text(currentQ.question);
+
 };
 
-function nextQBTN() {
-    $(".btn").on("click", function () {
-    }
-    )
-};
+answerButtons.on("click", function () {
+    currentIndex++;
+    qGenerator();
+});
+
 //just placeholder idea for time reduction on wrong answer
 function wrongReduce() {
     (timeLeft - (15 + seconds))
 };
 
 //set local storage for highscore
-
+qGenerator();
 // set highscores on said page
 
 // not important work on progress bar
